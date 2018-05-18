1405e62813e2e5dec77842426d30eed35e30f9d5
Comcx <comcx@outlook.com>
2018-05-18 21:31:42 +0800

    lex updated

---
+++ a_lex.c
@@ -0,0 +1,45 @@
+
+
+#include "a_lex.h"
+
+
+#define next(ls) ((ls)->current = as_getc((ls)->io))
+
+
+
+static const char *asX_tokens[] = {
+
+    "and", "break", "do", "else", "elseif",
+    "end", "false", "for", "function", "goto", "if",
+    "in", "local", "nil", "not", "or", "repeat",
+    "return", "then", "true", "until", "while",
+    "//", "..", "...", "==", ">=", "<=", "~=",
+    "<<", ">>", "::", "<eof>",
+    "<number>", "<integer>", "<name>", "<string>"
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- a_lex.h
+++ a_lex.h
@@ -9,6 +9,26 @@
 #include "a_io.h"
 
 
+typedef enum as_reserved {
+
+    /* terminal symbols denoted by reserved words */
+    TK_AND = FIRST_RESERVED, TK_BREAK,
+    TK_DO, TK_ELSE, TK_ELSEIF, TK_END, TK_FALSE, TK_FOR, TK_FUNCTION,
+    TK_GOTO, TK_IF, TK_IN, TK_LOCAL, TK_NIL, TK_NOT, TK_OR, TK_REPEAT,
+    TK_RETURN, TK_THEN, TK_TRUE, TK_UNTIL, TK_WHILE,
+    /* other terminal symbols */
+    TK_IDIV, TK_CONCAT, TK_DOTS, TK_EQ, TK_GE, TK_LE, TK_NE,
+    TK_SHL, TK_SHR,
+    TK_DBCOLON, TK_EOS,
+    TK_FLT, TK_INT, TK_NAME, TK_STRING
+
+};
+
+#define NUM_RESERVED (cast(int, TK_WHILE - FIRST_RESERVED + 1))
+
+
+
+
 typedef struct SemInfo {
 
     as_Number r;
