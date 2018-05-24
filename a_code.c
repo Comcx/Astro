120b5e0e953cb9425e1cdc7fd2c8e34e55ce3174
Comcx <comcx@outlook.com>
2018-05-24 14:47:24 +0800

    instruction formats added

--- a_code.c
+++ a_code.c
@@ -37,7 +37,7 @@ const char* const asC_OpName[NUM_OPCODE] = {
 "TAILCALL",
 "RETURN", 
 "FORLOOP",
-"FORREP",
+"FORPREP",
 "TFORLOOP",
 "SETLIST",
 "CLOSE",
@@ -63,7 +63,35 @@ OpFormat(0, 1, OpArgK, OpArgN, iABx),   /*OP_GETGLOBAL*/
 OpFormat(0, 1, OpArgR, OpArgK, iABC),   /*OP_GETTABLE*/
 OpFormat(0, 1, OpArgK, OpArgU, iABx),   /*OP_SETGLOBAL*/
 OpFormat(0, 1, OpArgU, OpArgN, iABC),   /*OP_SETUPVAL*/
-
+OpFormat(0, 1, OpArgK, OpArgK, iABC),   /*OP_SETTABLE*/
+OpFormat(0, 1, OpArgU, OpArgU, iABC),   /*OP_NEWTABLE*/
+OpFormat(0, 1, OpArgR, OpArgK, iABC),   /*OP_SELF*/
+OpFormat(0, 1, OpArgK, OpArgK, iABC),   /*OP_ADD*/
+OpFormat(0, 1, OpArgK, OpArgK, iABC),   /*OP_SUB*/
+OpFormat(0, 1, OpArgK, OpArgK, iABC),   /*OP_MUL*/
+OpFormat(0, 1, OpArgK, OpArgK, iABC),   /*OP_DIV*/
+OpFormat(0, 1, OpArgK, OpArgK, iABC),   /*OP_MOD*/
+OpFormat(0, 1, OpArgK, OpArgK, iABC),   /*OP_POW*/
+OpFormat(0, 1, OpArgR, OpArgN, iABC),   /*OP_UNM*/
+OpFormat(0, 1, OpArgR, OpArgN, iABC),   /*OP_NOT*/
+OpFormat(0, 1, OpArgR, OpArgN, iABC),   /*OP_LEN*/
+OpFormat(0, 1, OpArgR, OpArgR, iABC),   /*OP_CONCAT*/
+OpFormat(0, 0, OpArgR, OpArgN, iAsBx),  /*OP_JMP*/
+OpFormat(1, 0, OpArgK, OpArgK, iABC),   /*OP_EQ*/
+OpFormat(1, 0, OpArgK, OpArgK, iABC),   /*OP_LT*/
+OpFormat(1, 0, OpArgK, OpArgK, iABC),   /*OP_LE*/
+OpFormat(1, 0, OpArgN, OpArgU, iABC),   /*OP_TEST*/
+OpFormat(1, 1, OpArgR, OpArgU, iABC),   /*OP_TESTSET*/
+OpFormat(0, 1, OpArgU, OpArgU, iABC),   /*OP_CALL*/
+OpFormat(0, 1, OpArgU, OpArgU, iABC),   /*OP_TAILCALL*/
+OpFormat(0, 0, OpArgU, OpArgU, iABC),   /*OP_RETURN?*/
+OpFormat(0, 1, OpArgR, OpArgN, iAsBx),  /*OP_FORLOOP*/
+OpFormat(0, 1, OpArgR, OpArgN, iAsBx),  /*OP_FORPREP*/
+OpFormat(0, 1, OpArgR, OpArgN, iAsBx),  /*OP_TFORLOOP*/
+OpFormat(0, 0, OpArgU, OpArgU, iABC),   /*OP_SETLIST*/
+OpFormat(0, 0, OpArgR, OpArgN, iABC),   /*OP_CLOSE*/
+OpFormat(0, 1, OpArgU, OpArgN, iABx),   /*OP_CLOSURE*/
+OpFormat(0, 1, OpArgU, OpArgN, iABC),   /*OP_VARARG*/
 
 };
 
